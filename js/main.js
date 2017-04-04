1a9f286a5eb4609addfbfeb71b3ae69475ea2ac8
Ruth Salvador <ruth.abby.sz@gmail.com>
2017-04-04 12:12:54 -0500

    cambiando alerts x toolAlert

--- js/main.js
+++ js/main.js
@@ -6,20 +6,26 @@ function validateForm(){
   var password = document.getElementById('input-password');
   var elegir = document.getElementById('elegir');
 
+  var toolDiv =  document.getElementsByClassName('input-box');
+  var toolAlert = document.createElement('span');
+
   /*** Validando campo nombre***/
     if(name.value.length == 0){
-        alert ('Por favor ingrese su nombre');
+        toolDiv[0].appendChild(toolAlert);
+        toolAlert.appendChild(document.createTextNode('Por favor ingrese su nombre'));
         name.focus();
         return false;
     }
 
     for (var i = 1; i< name.value.length ; i++){
       if( !(/[A-ZÑÁÉÍÓÚa-zñáéíóú]/.test(name.value.split("")[i]))){ //for para recorrer los caracteres de name.value.split (array), me gusta mas split-array q charAt
-        alert ('El nombre debe contener solamente letras (a-z), por favor intente de nuevo');
+        toolDiv[0].appendChild(toolAlert);
+        toolAlert.appendChild(document.createTextNode('El nombre debe contener solamente letras (a-z), por favor intente de nuevo'));
         name.focus();
         return false;
       } else if( !(/[A-ZÑÁÉÍÓÚ]/.test(name.value.split("")[0]))){ //for para recorrer los caracteres de name.value.split (array), me gusta mas split-array q charAt
-        alert ('El nombre debe empezar en mayúscula, por favor intente de nuevo');
+        toolDiv[0].appendChild(toolAlert);
+        toolAlert.appendChild(document.createTextNode('El nombre debe empezar en mayúscula, por favor intente de nuevo'));
         name.focus();
         return false;
       }
@@ -27,18 +33,21 @@ function validateForm(){
 
   /*** Validando campo apellido***/
     if(lastname.value.length == 0){
-        alert ('Por favor ingrese su apellido');
+        toolDiv[1].appendChild(toolAlert);
+        toolAlert.appendChild(document.createTextNode('Por favor ingrese su apellido'));
         lastname.focus();
         return false;
     }
 
     for (var j = 1; j< lastname.value.length ; j++){
       if( !(/[A-ZÑÁÉÍÓÚa-zñáéíóú]/.test(lastname.value.split("")[j]))){
-        alert ('El apellido debe contener solamente letras (a-z), por favor intente de nuevo');
+        toolDiv[1].appendChild(toolAlert);
+        toolAlert.appendChild(document.createTextNode('El apellido debe contener solamente letras (a-z), por favor intente de nuevo'));
         lastname.focus();
         return false;
       } else if( !(/[A-ZÑÁÉÍÓÚ]/.test(lastname.value.split("")[0]))){
-        alert ('El apellido debe empezar en mayúscula, por favor intente de nuevo');
+        toolDiv[1].appendChild(toolAlert);
+        toolAlert.appendChild(document.createTextNode('El apellido debe empezar en mayúscula, por favor intente de nuevo'));
         lastname.focus();
         return false;
       }
@@ -46,33 +55,39 @@ function validateForm(){
 
   /*** Validando campo email***/
     if(email.value.length == 0){
-      alert ('Por favor ingrese su correo electrónico');
+      toolDiv[2].appendChild(toolAlert);
+      toolAlert.appendChild(document.createTextNode('Por favor ingrese su correo electrónico'));
       email.focus();
       return false;
     } else if (!(/[\w-\.]{3,}@[\w-]{3,}\.[A-Za-z]{2,3}/.test(email.value))){
-      alert ('El correo electrónico debe tener la siguiente forma email@dominio.com, por favor intente de nuevo');
+      toolDiv[2].appendChild(toolAlert);
+      toolAlert.appendChild(document.createTextNode('El correo electrónico debe tener la siguiente forma email@dominio.com, por favor intente de nuevo'));
       email.focus();
       return false;
     }
 
   /*** Validando campo password***/
     if(password.value.length == 0){
-      alert ('Por favor ingrese una contraseña');
+      toolDiv[3].appendChild(toolAlert);
+      toolAlert.appendChild(document.createTextNode('Por favor ingrese su contraseña'));
       password.focus();
       return false;
     } else if (password.value.length < 6) {
-      alert ('La contraseña debe tener mínimo 6 caracteres');
+      toolDiv[3].appendChild(toolAlert);
+      toolAlert.appendChild(document.createTextNode('Por favor ingrese su contraseña'));
       password.focus();
       return false;
     } else if ((password.value == "password") || (password.value == "123456") || (password.value =="098754")){
-      alert ('La contraseña no es segura, por favor intente de nuevo');
+      toolDiv[3].appendChild(toolAlert);
+      toolAlert.appendChild(document.createTextNode('La contraseña no es segura, por favor intente de nuevo'));
       password.focus();
       return false;
     }
 
   /*** Validando campo select/elegir***/
     if(elegir.value == 0){
-      alert ('Por favor, seleccione su bicicleta');
+      toolDiv[4].appendChild(toolAlert);
+      toolAlert.appendChild(document.createTextNode('Por favor, seleccione su bicicleta'));
       elegir.focus();
       return false;
     }
